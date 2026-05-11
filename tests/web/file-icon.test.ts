02ae51dee28944ec83f3ce8d62c6b2ec38ca1942
Almas Akchabayev <6459157+almeynman@users.noreply.github.com>
2026-05-11 09:24:19 +0200

    test(webapp): align file-icon test with File* icon swap

--- tests/web/file-icon.test.ts
+++ tests/web/file-icon.test.ts
@@ -1,23 +1,18 @@
 import { describe, it, expect } from "vitest";
-import {
-  DiffAddedIcon,
-  DiffModifiedIcon,
-  DiffRemovedIcon,
-  DiffRenamedIcon,
-} from "@primer/octicons-react";
+import { FileAddedIcon, FileDiffIcon, FileMovedIcon, FileRemovedIcon } from "@primer/octicons-react";
 import { fileIcon } from "../../src/web/client/file-icon.js";
 
 describe("fileIcon", () => {
   it.each([
-    ["new", DiffAddedIcon, "added"],
-    ["add", DiffAddedIcon, "added"],
-    ["change", DiffModifiedIcon, "modified"],
-    ["modify", DiffModifiedIcon, "modified"],
-    ["deleted", DiffRemovedIcon, "deleted"],
-    ["delete", DiffRemovedIcon, "deleted"],
-    ["rename", DiffRenamedIcon, "renamed"],
-    ["rename-pure", DiffRenamedIcon, "renamed"],
-    ["rename-changed", DiffRenamedIcon, "renamed"],
+    ["new", FileAddedIcon, "added"],
+    ["add", FileAddedIcon, "added"],
+    ["change", FileDiffIcon, "modified"],
+    ["modify", FileDiffIcon, "modified"],
+    ["deleted", FileRemovedIcon, "deleted"],
+    ["delete", FileRemovedIcon, "deleted"],
+    ["rename", FileMovedIcon, "renamed"],
+    ["rename-pure", FileMovedIcon, "renamed"],
+    ["rename-changed", FileMovedIcon, "renamed"],
   ])("maps %s -> (%s, %s)", (type, expectedIcon, expectedClass) => {
     const { Icon, statusClass } = fileIcon(type);
     expect(Icon).toBe(expectedIcon);
@@ -26,7 +21,7 @@ describe("fileIcon", () => {
 
   it("falls back to modified for unknown types", () => {
     const { Icon, statusClass } = fileIcon("totally-unknown");
-    expect(Icon).toBe(DiffModifiedIcon);
+    expect(Icon).toBe(FileDiffIcon);
     expect(statusClass).toBe("modified");
   });
 });
